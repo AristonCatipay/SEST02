4bc59d6dad0f0090a91ab4bd7a07b3d75ff83fc4
AristonCatipay <aristoncatipay123@gmail.com>
2025-01-14 22:28:12 +0800

    Finish the todolist application.

--- 15 - React and Vite/todolist-application/src/components/TodoList.jsx
+++ 15 - React and Vite/todolist-application/src/components/TodoList.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import '../App.css'
 
 function TodoList() {
   // [] - todo Array
   // ['Todo1', 'Todo2', 'Todo3']
+  // [{text: inputValue, completed: boolean}, {...}, {...}]
+
+  // {} - JavaScript Code (Code Block)
+  // () - JavaScript XML (Combination of JavaScript and HTML)
 
   // useState()
   // [element1, element2]
@@ -12,30 +17,83 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-  // inputValue = "Todo 2"
-  // todos = ["Todo 1", "Todo 2"]
-
-
   const addTodo = () => {
     if (inputValue.trim()) {
-        setTodos([...todos, inputValue]);
-        setInputValue("");
+      // 1. Using push
+      // todos.push(inputValue);
+      // setTodos([...todos]);
+
+      const newTodoObject = {
+        text: inputValue,
+        completed: false,
+      };
+
+      // 2. Using spread operator
+      setTodos([...todos, newTodoObject]);
+      setInputValue("");
+      // N: We need to pass a new array whenever we want to update.
     }
-  }
+  };
+
+  const deleteTodo = (index) => {
+    // .filter(): create a new array with elements that passed the test condition.
+    // Syntax: .filter((currentElement, currentElementIndex) => {})
+
+    // _ as a paramater name, it means you didn't use that paramater.
+    const updatedTodos = todos.filter(
+      (_, currentTodoIndex) => currentTodoIndex !== index
+    );
+    setTodos(updatedTodos);
+  };
+
+  // Old: ["Todo 1", "Todo 2"]
+  // Index: 1
+  // New: ["Todo 1"]
+
+  /*
+    .map(): create a new array by applying a function to each element of the original array
+    Syntax: .map((currentElement, currentElementIndex) => {})
+  */
+
+  const toggleTodoCompletion = (index) => {
+    const updatedTodos = todos.map((currentTodoObject, currentTodoIndex) => {
+      if (currentTodoIndex === index) {
+        const newTodoObject = {
+          ...currentTodoObject,
+          completed: !currentTodoObject.completed,
+        };
+        return newTodoObject;
+      } else {
+        return currentTodoObject;
+      }
+    });
+    setTodos(updatedTodos)
+  };
 
   return (
-    <div>
+    <div className="todo-list">
       <input
         type="text"
         placeholder="Enter a todo..."
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
       />
-      <button onClick={addTodo}>Add</button>
+      <button className="add-btn" onClick={addTodo}>Add</button>
 
       <ul>
-        {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
+        {todos.map((currentTodoObject, currentTodoObjectIndex) => (
+          <li key={currentTodoObjectIndex}>
+            <input
+              type="checkbox"
+              checked={currentTodoObject.completed}
+              onChange={() => toggleTodoCompletion(currentTodoObjectIndex)}
+            />
+
+            {currentTodoObject.text}
+            <button className="delete-btn" onClick={() => deleteTodo(currentTodoObjectIndex)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
